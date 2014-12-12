a90fe2d0c950b8e9390414e953b858639f675266
beh <beh@harpoon.cs.arizona.edu>
2014-12-12 14:55:02 -0700

    Added Some Classes

---
+++ Team Unknown/src/view/PlayerInfoPanel.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+public class PlayerInfoPanel extends JPanel implements Observer{
+
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
---
+++ Team Unknown/src/view/TowerSelectionPanel.java
@@ -0,0 +1,10 @@
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+public class TowerSelectionPanel extends JPanel {
+
+}
